98ca87c6e857521facf9bd7a746f1714608d277c
Shubham Upadhyay <iamshubhamupadhyaya@gmail.com>
2023-04-10 23:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const btnsxyz = Array.from(document.querySelectorAll(".btn"))
+function stopSounds(){
+    for(var i=0;i<btnsxyz.length;i++){
+        const sound = document.getElementById(btnsxyz[i].innerText)
+        sound.pause()
+        sound.currentTime = 0
+    }
+}
+function playSound(btn){
+    btn.addEventListener("click",function(){
+        stopSounds()
+        document.getElementById(btn.innerText).play()
+    })
+}
+for(var i=0;i<btnsxyz.length;i++){
+    var buttonxyz = btnsxyz[i]
+    playSound(buttonxyz)
+}
+document.querySelector(".stop").addEventListener("click",function(){
+    stopSounds()
+})
